3f5ac3ee02c5493c056416a457716c203a6582dc
santosh kumar mohapatra <santosh.mohapatra25@.com>
2021-11-08 07:02:06 +0000

    init

---
+++ hello-world/hello.js
@@ -0,0 +1,16 @@
+// Lambda function code
+
+module.exports.handler = async (event) => {
+  console.log('Event: ', event);
+  let responseMessage = 'Hello, World!-SANTOSH';
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      message: responseMessage,
+    }),
+  }
+}
