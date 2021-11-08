79262e861998079e29e207d3452e5be3dbeabd81
santosh kumar mohapatra <santosh.mohapatra25@.com>
2021-11-08 10:15:06 +0000

    test

--- main.tf
+++ main.tf
@@ -38,8 +38,16 @@ data "archive_file" "lambda_hello_world" {
   type = "zip"
 
   source_dir  = "${path.module}/backend/Nodejs"
-  output_path = "${path.module}//backend/Nodejs/hello-world.zip"
+  output_path = "${path.module}/backend/hello-world.zip"
 }
+
+data "archive_file" "lambda_hello_world-py" {
+  type = "zip"
+
+  source_dir  = "${path.module}/backend/Python"
+  output_path = "${path.module}/backend/hello-world-py.zip"
+}
+
 
 resource "aws_s3_bucket_object" "lambda_hello_world" {
   bucket = aws_s3_bucket.lambda_bucket.id
@@ -48,6 +56,15 @@ resource "aws_s3_bucket_object" "lambda_hello_world" {
   source = data.archive_file.lambda_hello_world.output_path
 
   etag = filemd5(data.archive_file.lambda_hello_world.output_path)
+}
+
+resource "aws_s3_bucket_object" "lambda_hello_world-py" {
+  bucket = aws_s3_bucket.lambda_bucket.id
+
+  key    = "hello-world-py.zip"
+  source = data.archive_file.lambda_hello_world-py.output_path
+
+  etag = filemd5(data.archive_file.lambda_hello_world-py.output_path)
 }
 
 
@@ -64,6 +81,21 @@ resource "aws_lambda_function" "hello_world" {
 
   role = aws_iam_role.lambda_exec.arn
 }
+
+resource "aws_lambda_function" "hello_world-py" {
+  function_name = "HelloWorld-py"
+
+  s3_bucket = aws_s3_bucket.lambda_bucket.id
+  s3_key    = aws_s3_bucket_object.lambda_hello_world-py.key
+
+  runtime = "python3.8"
+  handler = "lambda_handler"
+
+  source_code_hash = data.archive_file.lambda_hello_world-py.output_base64sha256
+
+  role = aws_iam_role.lambda_exec.arn
+}
+
 
 resource "aws_cloudwatch_log_group" "hello_world" {
   name = "/aws/lambda/${aws_lambda_function.hello_world.function_name}"
